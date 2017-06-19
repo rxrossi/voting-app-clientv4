e316c55f87344e79408c8d227d939287cf048c41
Alex Rossi <rx.rossi@gmail.com>
2017-06-19 10:04:26 -0300

    made some improvements on VotelPoll.js

--- src/components/VotePoll.js
+++ src/components/VotePoll.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { Ul, Li, Button, Input } from './Shared';
+import { Ul, Li, Button, Input, Form } from './Shared';
 
-export default ({ poll, authenticated, hasVoted }) => {
-	if (hasVoted) {
-		return (
-			<Ul>
-				{poll.opts.map(opt =>
-					<Li>
-						{opt.name} - {opt.votes}
-					</Li>
-				)}
-			</Ul>
-		);
+export default class VotePoll extends React.Component {
+	state = {
+
 	}
-	return (
-		<div>
-			<p>Select one to vote or create a new option</p>
-			<Ul>
-				{poll.opts.map(opt =>
-					<Li>
-						{opt.name}
-					</Li>
-				)}
-			</Ul>
-			<Input placeholder="Name"/>
-			<Button small type="button">+</Button>
-		</div>
-	);
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.newOpt(this.state.input)
+	}
+	vote = (name) => {
+
+	}
+	changeInput = (e) => {
+		this.setState({ input: e.target.value })
+	}
+	render() {
+		const { poll, authenticated, hasVoted } = this.props;
+		if (hasVoted) {
+			return (
+				<Ul>
+					{poll.opts.map(opt =>
+						<Li>
+							{opt.name} - {opt.votes}
+						</Li>
+					)}
+				</Ul>
+			);
+		}
+		return (
+			<div>
+				<p>Select one to vote or create a new option</p>
+				<Ul>
+					{poll.opts.map(opt =>
+						<Li onClick={() =>this.vote(opt.name)}>
+							{opt.name}
+						</Li>
+					)}
+				</Ul>
+				<Form onSubmit={this.onSubmit}>
+					<Input
+						placeholder="The name of a new option"
+						onChange={this.changeInput}
+					/>
+					<Button small type="button" nature="primary">+</Button>
+				</Form>
+			</div>
+		);
+
+	}
 }
